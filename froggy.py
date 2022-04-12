950487cc6f644bb57eaa29d06f5bb423e184a4c0
QuantumOctopus <rithviksub@gmail.com>
2022-04-12 19:19:13 -0400

    added function to print members

--- froggy.py
+++ froggy.py
@@ -12,6 +12,14 @@ class MyClient(discord.Client):
 
     #async def on_member_join(self, member):
 
+    @client.event
+	async def on_message(message):
+    	if message.content.startswith('!member'):
+        	for guild in client.guilds:
+            	for member in guild.members:
+                	print(member)
+
+
 
 client = MyClient()
 client.run('OTYzNTYzODUyNDcyMTQ3OTk4.YlX6xA.hjhom0dPsoPJybXnmjJGpf0Sdx8')
