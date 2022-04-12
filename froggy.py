cfc50e2ba9bb5f9965f7d253cf83b1869c692ee4
Wekker1 <veopress@gmail.com>
2022-04-12 18:28:34 -0500

    fix qo stuff

--- froggy.py
+++ froggy.py
@@ -13,16 +13,14 @@ class MyClient(discord.Client):
         print('Logged on as {0}!'.format(self.user))
 
     async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+        if message.content.startswith('!member'):
+            for guild in client.guilds:
+                for member in guild.members:
+                    print(guild.name + " s " + member.name)
 
     #async def on_member_join(self, member):
 
-    @client.event
-	async def on_message(message):
-    	if message.content.startswith('!member'):
-        	for guild in client.guilds:
-            	for member in guild.members:
-                	print(member)
+    	
 
 
 
