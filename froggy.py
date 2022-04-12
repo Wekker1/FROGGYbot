3dbe21cf59b21df7baa49579db3ec702ac28a791
Wekker1 <veopress@gmail.com>
2022-04-12 18:16:03 -0500

    first commit :)

---
+++ froggy.py
@@ -0,0 +1,17 @@
+import discord
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+    #async def on_member_join(self, member):
+
+
+client = MyClient()
+client.run('OTYzNTYzODUyNDcyMTQ3OTk4.YlX6xA.hjhom0dPsoPJybXnmjJGpf0Sdx8')
