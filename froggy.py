0be11b70d28abe6e28d47eb4e9070388ea48458f
Wekker1 <veopress@gmail.com>
2022-04-12 19:00:08 -0500

    Change Roles :)

--- froggy.py
+++ froggy.py
@@ -1,6 +1,8 @@
 import discord
 import logging
 from discord.ext import commands
+from discord.utils import get
+import random
 import os
 from dotenv import load_dotenv
 
@@ -8,21 +10,33 @@ load_dotenv()
 
 logging.basicConfig(level=logging.INFO)
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
-
-    async def on_message(self, message):
-        if message.content.startswith('!member'):
-            for guild in client.guilds:
-                for member in guild.members:
-                    print(guild.name + " s " + member.name)
-
-    #async def on_member_join(self, member):
-
-    	
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='?', intents = intents)
 
 
+random_role_names = [
+'Test 1',
+'Test 2',
+'Test 3',
+]
 
-client = MyClient()
-client.run(os.getenv("DISCORD_TOKEN"))
+@bot.event
+async def on_ready():
+    print('Logged on as {0}!'.format(bot.user))
+
+@bot.event
+async def on_message(message):
+    if message.content.startswith('!role'):
+        member = message.author
+        role = get(member.guild.roles, name=random_role_names[random.randint(0,2)])
+        print(member.name + " given role: " + role.name)
+        await member.add_roles(role)
+
+@bot.event
+async def on_member_join(member):
+    print(member.name + " joined!")
+    role = get(member.guild.roles, name=random_role_names[random.randint(0,2)])
+    await member.add_roles(role)
+
+
+bot.run(os.getenv("DISCORD_TOKEN"))
